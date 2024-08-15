ebccdd85640ee39e8006041f054c49d7feac9741
cpraveenc <140836209+cpraveenc@users.noreply.github.com>
2024-08-15 07:30:07 -0400

    Update emr_pipeline_stack.py

--- emr_pipeline_stack.py
+++ emr_pipeline_stack.py
@@ -1,68 +1,105 @@
-import os
 from aws_cdk import (
-    Stack, aws_s3 as s3, aws_sqs as sqs, aws_lambda as _lambda,
-    aws_s3_notifications as s3n, aws_iam as iam, aws_emr as emr, aws_lambda_event_sources as _lambda_event_sources
+    core as cdk,
+    aws_s3 as s3,
+    aws_lambda as _lambda,
+    aws_sqs as sqs,
+    aws_s3_notifications as s3n,
+    aws_iam as iam,
+    aws_emr as emr,
+    aws_events as events,
+    aws_events_targets as targets,
 )
-from constructs import Construct
-from dotenv import load_dotenv
+import os
 
-class MyPipelineStack(Stack):
+class PipelineStack(cdk.Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: cdk.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
         # Load environment variables
-        load_dotenv()
+        input_bucket_name = os.getenv('INPUT_BUCKET_NAME')
+        output_bucket_name = os.getenv('OUTPUT_BUCKET_NAME')
+        lambda_timeout = int(os.getenv('LAMBDA_TIMEOUT', 300))
+        
+        # S3 Buckets
+        input_bucket = s3.Bucket(self, "InputBucket", bucket_name=input_bucket_name)
+        output_bucket = s3.Bucket(self, "OutputBucket", bucket_name=output_bucket_name)
 
-        # Fetch bucket names from environment variables
-        source_bucket_name = os.getenv('SOURCE_BUCKET_NAME', 'default-source-bucket')
-        target_bucket_name = os.getenv('TARGET_BUCKET_NAME', 'default-target-bucket')
+        # SQS Queue
+        queue = sqs.Queue(self, "FileProcessingQueue")
 
-        # Create Source S3 Bucket
-        source_bucket = s3.Bucket(self, "SourceBucket",
-                                  bucket_name=source_bucket_name)
+        # Lambda Function to Trigger EMR Cluster
+        emr_lambda = _lambda.Function(
+            self, 
+            "EmrLambdaFunction",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler="emr_lambda.handler",
+            code=_lambda.Code.from_asset("lambda"),
+            timeout=cdk.Duration.seconds(lambda_timeout),
+            environment={
+                "OUTPUT_BUCKET_NAME": output_bucket.bucket_name,
+            }
+        )
 
-        # Create Target S3 Bucket for processed data
-        target_bucket = s3.Bucket(self, "TargetBucket",
-                                  bucket_name=target_bucket_name)
+        # Permissions for Lambda to access S3 and EMR
+        emr_lambda.add_to_role_policy(
+            iam.PolicyStatement(
+                actions=["s3:*", "elasticmapreduce:*"],
+                resources=["*"]
+            )
+        )
 
-        # Create SQS Queue
-        queue = sqs.Queue(self, "S3ToSQSQueue")
-
-        # Add S3 Event Notification to SQS for new object uploads
-        source_bucket.add_event_notification(
+        # S3 Notification to SQS
+        input_bucket.add_event_notification(
             s3.EventType.OBJECT_CREATED,
             s3n.SqsDestination(queue)
         )
 
-        # Create IAM Role for Lambda to interact with S3 and EMR
-        lambda_role = iam.Role(self, "LambdaExecutionRole",
-            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
-            managed_policies=[
-                iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole"),
-                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonEMRFullAccessPolicy_v2"),
-                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess")
-            ]
+        # SQS Trigger for Lambda
+        emr_lambda.add_event_source(_lambda_event_sources.SqsEventSource(queue))
+
+        # IAM Role for EMR
+        emr_role = iam.Role(self, "EMRRole", 
+                            assumed_by=iam.ServicePrincipal("elasticmapreduce.amazonaws.com"),
+                            managed_policies=[
+                                iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonElasticMapReduceRole")
+                            ])
+
+        # EMR Cluster
+        cluster = emr.CfnCluster(
+            self, 
+            "EmrCluster",
+            name="DataProcessingCluster",
+            release_label="emr-6.7.0",
+            instances=emr.CfnCluster.JobFlowInstancesConfigProperty(
+                master_instance_group=emr.CfnCluster.InstanceGroupConfigProperty(
+                    instance_count=1, instance_type="m5.xlarge"
+                ),
+                core_instance_group=emr.CfnCluster.InstanceGroupConfigProperty(
+                    instance_count=2, instance_type="m5.xlarge"
+                ),
+                ec2_key_name="your-ec2-key-name"
+            ),
+            job_flow_role=emr_role.role_name,
+            service_role=emr_role.role_name
         )
 
-        # Create Lambda Function to trigger EMR cluster
-        emr_lambda = _lambda.Function(self, "EMRClusterTriggerLambda",
-            runtime=_lambda.Runtime.PYTHON_3_9,
-            handler="lambda_function.handler",
-            code=_lambda.Code.from_asset("lambda"),  # Path to the lambda folder
-            role=lambda_role,
-            environment={
-                'SOURCE_BUCKET': source_bucket.bucket_name,
-                'TARGET_BUCKET': target_bucket.bucket_name
+        # Trigger EMR Cluster completion event to handle processing completion
+        emr_completion_rule = events.Rule(
+            self, "EMRCompletionRule",
+            event_pattern={
+                "source": ["aws.emr"],
+                "detail-type": ["EMR Cluster State Change"],
+                "detail": {
+                    "state": ["TERMINATED"]
+                }
             }
         )
 
-        # Grant Lambda permissions to read from SQS and access the S3 buckets
-        queue.grant_consume_messages(emr_lambda)
-        source_bucket.grant_read(emr_lambda)
-        target_bucket.grant_write(emr_lambda)
-
-        # Set SQS as the trigger for Lambda
-        emr_lambda.add_event_source(
-            _lambda_event_sources.SqsEventSource(queue)
+        emr_completion_rule.add_target(
+            targets.LambdaFunction(emr_lambda)
         )
+
+        # Grant permissions
+        input_bucket.grant_read(emr_lambda)
+        output_bucket.grant_write(emr_lambda)
