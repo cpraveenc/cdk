c317fa31610307cfc8a97f866188ba4cece3def1
cpraveenc <140836209+cpraveenc@users.noreply.github.com>
2024-08-15 07:30:33 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,62 +1,54 @@
 import boto3
 import os
-import json
 
-s3_client = boto3.client('s3')
 emr_client = boto3.client('emr')
+s3_client = boto3.client('s3')
 
 def handler(event, context):
-    # Get the bucket names from environment variables
-    source_bucket_name = os.environ['SOURCE_BUCKET']
-    target_bucket_name = os.environ['TARGET_BUCKET']
-
-    # Process SQS messages
-    for record in event['Records']:
-        payload = json.loads(record['body'])
-        object_key = payload['Records'][0]['s3']['object']['key']
-
-        # Trigger EMR Cluster
-        cluster_id = emr_client.run_job_flow(
-            Name='MyEMRCluster',
-            ReleaseLabel='emr-6.3.0',
-            Instances={
-                'InstanceGroups': [
-                    {
-                        'Name': 'Master nodes',
-                        'Market': 'ON_DEMAND',
-                        'InstanceRole': 'MASTER',
-                        'InstanceType': 'm5.xlarge',
-                        'InstanceCount': 1,
-                    },
-                    {
-                        'Name': 'Core nodes',
-                        'Market': 'ON_DEMAND',
-                        'InstanceRole': 'CORE',
-                        'InstanceType': 'm5.xlarge',
-                        'InstanceCount': 2,
-                    }
-                ],
-                'Ec2KeyName': 'your-keypair-name',
-                'KeepJobFlowAliveWhenNoSteps': False,
-                'TerminationProtected': False
-            },
-            Steps=[{
-                'Name': 'ProcessFiles',
-                'ActionOnFailure': 'CONTINUE',
-                'HadoopJarStep': {
-                    'Jar': 'command-runner.jar',
-                    'Args': [
-                        's3-dist-cp',
-                        '--src', f's3://{source_bucket_name}/{object_key}',
-                        '--dest', f's3://{target_bucket_name}/processed/'
-                    ]
-                }
-            }],
-            JobFlowRole='EMR_EC2_DefaultRole',
-            ServiceRole='EMR_DefaultRole'
-        )
-
+    # Retrieve the output bucket name from environment variables
+    output_bucket_name = os.getenv('OUTPUT_BUCKET_NAME')
+    
+    # Here, you will trigger your EMR cluster or handle completion
+    if event.get("detail-type") == "EMR Cluster State Change" and event["detail"]["state"] == "TERMINATED":
+        # Handle EMR Cluster completion
+        # This is where the processed files would be moved to the output S3 bucket
+        s3_client.put_object(Bucket=output_bucket_name, Key="result.csv", Body=b"Processed data...")
         return {
             'statusCode': 200,
-            'body': json.dumps(f'Cluster created with ID: {cluster_id["JobFlowId"]}')
+            'body': 'Processing complete and result stored in output S3 bucket.'
         }
+    
+    # Logic for starting the EMR Cluster
+    response = emr_client.run_job_flow(
+        Name='MyJobFlow',
+        Instances={
+            'InstanceGroups': [
+                {
+                    'Name': 'Master nodes',
+                    'Market': 'ON_DEMAND',
+                    'InstanceRole': 'MASTER',
+                    'InstanceType': 'm5.xlarge',
+                    'InstanceCount': 1,
+                },
+                {
+                    'Name': 'Core nodes',
+                    'Market': 'ON_DEMAND',
+                    'InstanceRole': 'CORE',
+                    'InstanceType': 'm5.xlarge',
+                    'InstanceCount': 2,
+                }
+            ],
+            'Ec2KeyName': 'your-ec2-key-name',
+            'KeepJobFlowAliveWhenNoSteps': False,
+        },
+        Steps=[],
+        JobFlowRole='EMR_EC2_DefaultRole',
+        ServiceRole='EMR_DefaultRole',
+        ReleaseLabel='emr-6.7.0',
+        LogUri=f's3://{output_bucket_name}/emr-logs/'
+    )
+    
+    return {
+        'statusCode': 200,
+        'body': f'EMR cluster created with id: {response["JobFlowId"]}'
+    }
